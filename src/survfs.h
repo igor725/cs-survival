d3641986ebee47fd6a4b1bff1a648e4251444364
igor725 <gvaldovigor@gmail.com>
2022-12-19 10:26:53 +0300

    Endianess detection

--- src/survfs.h
+++ src/survfs.h
@@ -3,7 +3,11 @@
 #include <core.h>
 #include "survdata.h"
 
-#define SURVFS_MAGIC 0xDEADBEEFu
+#ifdef CORE_USE_LITTLE
+#	define SURVFS_MAGIC 0xEFBEADDEu
+#else
+#	define SURVFS_MAGIC 0xDEADBEEFu
+#endif
 
 void SurvFS_Init(void);
 cs_bool SurvFS_LoadPlayerData(SrvData *data);
